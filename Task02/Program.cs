2944b7c50583711db7f1d70b8dbe55e2ba9c7cdf
Pavel <Shestakov1996@yandex.ru>
2022-10-16 11:37:33 +0300

    Внесли изменения в задачу №2

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -7,14 +7,6 @@ int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число №2:     ");
 int b = Convert.ToInt32(Console.ReadLine());
 
-int max = a;
-int min = b;
-
-if (a > b) Console.Write($"Максимальное число: {max} , Минимальное число: {min}");
-if (b > a)
-{
-    max = b;
-    min = a;
-    Console.Write($"Максимальное число: {max} , Минимальное число: {min}");
-}
+if (a > b) Console.Write($"Максимальное число: {a}  Минимальное число: {b}");
+if (b > a)  Console.Write($"Максимальное число: {b}  Минимальное число: {a}");
 if (a == b) Console.Write("Числа равны");
