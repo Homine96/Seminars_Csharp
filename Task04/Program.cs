32c3c51f04a7e72bd85ba7d9242dbb3f731f852a
Pavel <Shestakov1996@yandex.ru>
2022-10-16 10:41:22 +0300

    Задача на нахождение маскимального из 3 чисел

---
+++ Task04/Program.cs
@@ -0,0 +1,21 @@
+﻿// Программа, которая на входи принимает три числа и выдает максимальное
+
+int Max (int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+Console.WriteLine("Введите число №1:     ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число №2:     ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число №1:     ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = Max (a, b, c);
+Console.WriteLine($"Максимальное число: {max}");
