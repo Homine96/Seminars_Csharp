46def421ebb11e9e331290a8c6cfa69eca96fffa
Pavel <Shestakov1996@yandex.ru>
2022-10-16 10:54:28 +0300

    Задача №3,проверка на четность

---
+++ Task06/Program.cs
@@ -0,0 +1,8 @@
+﻿//Программа, которая на входи принимает число и выдает является ли оно четным
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0) Console.WriteLine("Число четное ");
+else Console.WriteLine ("Число нечетное");
+
