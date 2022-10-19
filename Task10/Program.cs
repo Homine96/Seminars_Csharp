d806f8c86bcfe14e301a80d2507cf3b6f9866491
Pavel <Shestakov1996@yandex.ru>
2022-10-19 23:39:42 +0300

    Задача 10

---
+++ Task10/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Задача 10. Напишите программу , которая на вход принимает трехзначное число и 
+на выходе показывает вторую цифру этого числа*/
+
+Console.Write("Введите трехзначное число:   ");
+int num =Convert.ToInt32(Console.ReadLine());
+
+int Number2(int num)
+{
+    int a = num / 10;
+    int b = a %10;
+    return b;
+}
+
+int number2=Number2(num);
+Console.WriteLine($"Вторая цифра {number2}");
