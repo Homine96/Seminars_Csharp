060f9ab99774eb154d1c0e06c3aaf7de3e39683b
Pavel <Shestakov1996@yandex.ru>
2022-10-20 10:52:35 +0300

    update Task13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -5,33 +5,15 @@
  32679 -> 6
 */
 
-// int Numb3(int a, int b)
-// {
-//     int result=0; 
-//     if (b < 3) Console.Write("Третьей цифры нет");
-//     else
-//     {
-
-//         int c = 1;
-//         while (b > 3)
-//         {
-//             c = c * 10;
-//             b--;
-//         }
-//          result = (a / c) % 10;
-//         Console.WriteLine($"Третья цифра : {result}");
-//     }
-//     return result;
-// }
-
-// Console.WriteLine("Введите число:    ");
-// int num = Convert.ToInt32(Console.ReadLine());
-// int integer = num.ToString().Length;
-// int final = Numb3(num, integer);
+void NumberDigit(int num)
+{
+    string numstr=Convert.ToString(num);
+    int numlength=num.ToString().Length;
+    if (numlength > 2) Console.WriteLine($"Третья цифра введённого числа: { numstr [2]}");
+    else Console.WriteLine("третьей цифры нет");
+}
 
 Console.Write("Введите любое число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-string numberstr = Convert.ToString(number);
-int dlinanumber = number.ToString().Length;
-if (dlinanumber > 2) Console.WriteLine($"Третья цифра введённого числа: { numberstr [2]}");
-else Console.WriteLine("третьей цифры нет");
+
+NumberDigit(number);
