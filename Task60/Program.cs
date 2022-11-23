58806d4ed92ea8b531e0a137d80e79bc9f48f6ef
Pavel <Shestakov1996@yandex.ru>
2022-11-23 13:51:44 +0300

    Задача №60

---
+++ Task60/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Например, задан массив размером 2 x 2 x 2.
+// Результат:
+// 66(0,0,0) 27(0,0,1) 25(0,1,0) 90(0,1,1)
+// 34(1,0,0) 26(1,0,1) 41(1,1,0) 55(1,1,1)
+
+int[,,] CreateMatrixRndInt3(int rows, int columns, int depth, int min, int max)
+{
+    int[,,] matrix = new int[rows, columns, depth]; // 0, 1
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                while (matrix[i, j, k] == 0)
+                {
+                    int number = rnd.Next(min, max + 1);
+                    if (IsNumberInArray(matrix, number) == false)
+                    {
+                        matrix[i, j, k] = number;
+                    }
+                }
+            }
+        }
+    }
+    return matrix;
+}
+
+bool IsNumberInArray(int[,,] array, int number)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                if (array[i, j, k] == number) return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+
+void PrintArray3(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write(array[i, j, k]);
+                Console.Write($"({i},{j},{k}) ");
+            }
+
+            // Console.WriteLine();
+        }
+
+        Console.WriteLine();
+    }
+}
+
+int[,,] array=CreateMatrixRndInt3(2,2,2,10,99);
+PrintArray3(array);
